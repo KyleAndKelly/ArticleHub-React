d2940f3e77f5d770ce3f53510b43363b7b325d2c
KyleAndKelly <2431173627@qq.com>
2024-12-28 14:45:10 +0900

    build login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,63 @@
+import './index.scss'
+import React from 'react';
+import { Card,Button, Checkbox, Form, Input } from 'antd';
+
+const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
 const Login = () => {
-    return <div>this is login</div>
+    return  <div className="login">
+
+    <Card
+      className="login-container"
+    >
+  <Form
+    name="basic"
+    initialValues={{
+      remember: true,
+    }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+    validateTrigger={['onBlur']}
+  >
+  <Form.Item
+      label="Username"
+      name="username"
+      
+      rules={[
+        {
+          required: true,
+          message: 'Please input your username!',
+        },
+      ]}
+    >
+      <Input placeholder= "please input username" />
+    </Form.Item>
+
+    <Form.Item
+      label="Password"
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
+    >
+      <Input.Password placeholder= "please input password"/>
+    </Form.Item>
+
+    <Form.Item label={null}>
+      <Button block type="primary" htmlType="submit">
+        Log in
+      </Button>
+    </Form.Item>
+  </Form>
+  </Card>
+    </div>
   }
   export default Login
