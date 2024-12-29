772f26d75b676308a7f5b016546197eef335de6b
KyleAndKelly <2431173627@qq.com>
2024-12-29 17:46:48 +0900

    add publish page

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -1,5 +1,78 @@
-const Publish = () => {
-    return <div>Publish</div>
-}
+import {
+    Card,
+    Breadcrumb,
+    Form,
+    Button,
+    Radio,
+    Input,
+    Upload,
+    Space,
+    Select
+  } from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import './index.scss'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; 
+  
+  const { Option } = Select
+  
+  const Publish = () => {
+    return (
+      <div className="publish">
+        <Card
+          title={
+            <Breadcrumb items={[
+              { title: <Link to={'/'}>首页</Link> },
+              { title: '发布文章' },
+            ]}
+            />
+          }
+        >
+          <Form
+            labelCol={{ span: 4 }}
+            wrapperCol={{ span: 16 }}
+            initialValues={{ type: 1 }}
+          >
+            <Form.Item
+              label="Title"
+              name="title"
+              rules={[{ required: true, message: 'Please input title!' }]}
+            >
+              <Input placeholder="Please input title" style={{ width: 400 }} />
+            </Form.Item>
+            <Form.Item
+              label="Channel"
+              name="channel_id"
+              rules={[{ required: true, message: 'Please select channel' }]}
+            >
+              <Select placeholder="Please select channel" style={{ width: 400 }}>
+                <Option value={0}>Recommend</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item
+              label="Content"
+              name="content"
+              rules={[{ required: true, message: 'Please input contents' }]}
+            ></Form.Item>
 
-export default Publish
+            <ReactQuill
+                    className="publish-quill"
+                    theme="snow"
+                    placeholder="请输入文章内容"
+            />
+
+            <Form.Item wrapperCol={{ offset: 4 }}>
+              <Space>
+                <Button size="large" type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Space>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
+    )
+  }
+  
+  export default Publish
